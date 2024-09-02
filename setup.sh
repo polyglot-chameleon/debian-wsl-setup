d41107c4d0bc04032e81737acb296aca92a79d76
polyglot-chameleon <61985465+polyglot-chameleon@users.noreply.github.com>
2024-09-02 18:15:53 +0200

    fix rbenv installation

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-
 sudo apt update
 
+### Deps
 sudo apt install -y git curl build-essential libz-dev libffi-dev libssl-dev libreadline-dev libyaml-dev wget
 
 
@@ -9,12 +9,13 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz 
 
-echo "export PATH=\"$PATH:/opt/nvim-linux64/bin\"" >> ~/.bashrc
+echo "export PATH=\"\$PATH:/opt/nvim-linux64/bin\"" >> ~/.bashrc
 
 ### Ruby Dev
+rm -rf ~/.rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 ~/.rbenv/bin/rbenv init
 
-exec $SHELL
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+exec $SHELL
