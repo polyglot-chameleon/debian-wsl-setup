6b4b8502cdd5da7baa572525944e469b1ea58c09
polyglot-chameleon <61985465+polyglot-chameleon@users.noreply.github.com>
2024-09-02 19:35:23 +0200

    add docker #2

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 sudo apt update
 
 ### Deps
-sudo apt install -y git curl build-essential libz-dev libffi-dev libssl-dev libreadline-dev libyaml-dev wget
-
+sudo apt install -y git curl ca-certificates build-essential libz-dev libffi-dev libssl-dev libreadline-dev libyaml-dev wget
+sudo install -m 0755 -d /etc/apt/keyrings
 
 ### Neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
@@ -17,5 +19,25 @@ git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 ~/.rbenv/bin/rbenv init
 
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+### Docker
+sudo sh -c "printf \"[boot]\nsystemd=true\n\" > /etc/wsl.conf"
+
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+### restart WSL ###
 
 exec $SHELL
