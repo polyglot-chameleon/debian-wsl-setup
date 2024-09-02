d8c65aa151f806affe42eff63fc5cc2f1eaf8dbe
polyglot-chameleon <oleg.harlamov@web.de>
2024-09-02 16:23:51 +0200

    init commit

---
+++ setup.sh
@@ -0,0 +1,18 @@
+
+sudo apt update
+
+sudo apt install git curl build-essential libz-dev libffi-dev
+
+
+### Neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz 
+
+echo "export PATH=\"$PATH:/opt/nvim-linux64/bin\"" >> ~/.bashrc
+
+### Ruby Dev
+git clone https://github.com/rbenv/rbenv.git
+~/.rbenv/bin/rbenv init
+
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
