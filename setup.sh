658adbc4f67f19ff4a8eed8514346827e124daa0
polyglot-chameleon <61985465+polyglot-chameleon@users.noreply.github.com>
2024-09-05 00:22:01 +0200

    fix tar gzip

--- setup.sh
+++ setup.sh
@@ -14,11 +14,11 @@ sudo tar -C /opt -xzf nvim-linux64.tar.gz
 echo "export PATH=\"\$PATH:/opt/nvim-linux64/bin\"" >> ~/.bashrc
 
 ### Go
-curl -O https:/go.dev/dl/go1.23.0.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.0.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+curl -LO https:/go.dev/dl/go1.23.0.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.23.0.linux-amd64.tar.gz
 echo "export PATH=\"\$PATH:/usr/local/go/bin\"" >> ~/.bashrc
 echo "export PATH=\"\$PATH:/usr/local/go/bin\"" >> ~/.zprofile
-
 
 ### Ruby Dev
 rm -rf ~/.rbenv
