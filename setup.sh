5782fb20381d9374ae6528657cfefc18dd8b565c
polyglot-chameleon <oleg.harlamov@web.de>
2024-09-11 01:15:15 +0200

    drop nvm for bun #14

--- setup.sh
+++ setup.sh
@@ -68,9 +68,9 @@ sudo tar -C /usr/local -xzf go1.23.0.linux-amd64.tar.gz
 echo "export PATH=\"\$PATH:/usr/local/go/bin\"" >> ~/.bashrc
 echo "export PATH=\"\$PATH:/usr/local/go/bin\"" >> ~/.zprofile
 
-### NVM
+### Bun
 zsh << EOF
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+curl -fsSL https://bun.sh/install | bash
 EOF
 
 ### Python3
