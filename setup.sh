f3d703ab1ffeb65b965dffcb3f0c8989879ed00f
polyglot-chameleon <61985465+polyglot-chameleon@users.noreply.github.com>
2024-09-12 22:06:25 +0200

    setup automatic new branch push #20 (#22)
    * add push.autoSetupRemote #20
    
    * chg default pull behav rebase

--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ sudo install -m 0755 -d /etc/apt/keyrings
 
 ### Git
 git config --global init.defaultBranch main
+git config --global push.autoSetupRemote true
+git config --global pull.rebase true
+
 
 git config --global user.name "$USERNAME"
 git config --global user.email $USERMAIL
